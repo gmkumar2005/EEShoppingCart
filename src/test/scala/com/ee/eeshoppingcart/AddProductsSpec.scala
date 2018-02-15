fc3870d7b29c13cf3297878e3a49363f02bb0bd7
Kiran Kumar <gmkumar2005@gmail.com>
2018-02-15 17:49:40 +0530

    Step 2 : Add additional products of the same type to the shopping cart.

--- src/test/scala/com/ee/eeshoppingcart/AddProductsSpec.scala
+++ src/test/scala/com/ee/eeshoppingcart/AddProductsSpec.scala
@@ -12,7 +12,7 @@ class AddProductsSpec extends FeatureSpec with GivenWhenThen with Matchers {
   info("then calculate the total price and tax amounts for the items contained in the cart")
 
   feature("Shopping Cart") {
-    scenario("Step 1 : Add products to the shopping cart") {
+    scenario("Step 1 : Add products to the shopping cart.") {
 
       Given("An empty shopping cart")
       val sc = new ShoppingCart()
@@ -35,6 +35,27 @@ class AddProductsSpec extends FeatureSpec with GivenWhenThen with Matchers {
       sc.getTotalPrice.doubleValue shouldEqual expectedTotalPrice
     }
 
+  }
+  scenario("Step 2: Add additional products of the same type to the shopping cart.") {
+    Given("An empty shopping cart")
+    val sc = new ShoppingCart()
+    And("a product, Dove Soap with a unit price of 39.99")
+    val doveSoapId = UUID.randomUUID()
+    val itemPrice = new java.math.BigDecimal(39.99).setScale(ShoppingCart.PRICE_SCALE, java.math.BigDecimal.ROUND_HALF_UP)
+    val doveSoap = new Product("Dove Soap", itemPrice, doveSoapId)
+
+    When("The user adds 5 Dove Soaps to the shopping cart")
+    sc.addItem(doveSoap.getProductName, doveSoap.getPrice, doveSoap.getProductId, 5)
+    And("then adds another 3 Dove Soaps to the shopping cart")
+    sc.addItem(doveSoap.getProductName, doveSoap.getPrice, doveSoap.getProductId, 3)
+
+    Then("The shopping cart should contain 8 Dove Soaps each with a unit price of 39.99")
+    sc.getItemCount should be(8)
+    And("the shopping cart’s total price should equal 319.92")
+    val expectedTotalPrice = new java.math.BigDecimal(319.92).setScale(ShoppingCart.PRICE_SCALE, java.math.BigDecimal.ROUND_HALF_UP).doubleValue
+    sc.getTotalPrice.doubleValue shouldEqual expectedTotalPrice
+    val shoppingItems = sc.getItems
+    val shoppingItems2 = sc.getItems
   }
 
 }
