bd8e0bf520b98209ee2771cfbe74e2f8e7308999
Kiran Kumar <gmkumar2005@gmail.com>
2018-02-15 20:33:35 +0530

    Step 3: Calculate the tax rate of the shopping cart with multiple items

--- src/main/java/com/ee/eeshoppingcart/ShoppingCart.java
+++ src/main/java/com/ee/eeshoppingcart/ShoppingCart.java
@@ -1,40 +1,72 @@
 package com.ee.eeshoppingcart;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class ShoppingCart {
 
   public static int PRICE_SCALE = 2;
 
-  public ShoppingCart() {
+  private Map<UUID, ShoppingItem> items = new HashMap<>();
+  private BigDecimal taxRate;
+
+  public void addItem(String productName, BigDecimal itemPrice, UUID productId, int itemQuantity) {
+    ShoppingItem existingItem = items.get(productId);
+    if (existingItem != null) {
+      ShoppingItem item = new ShoppingItem(existingItem.getProductName(),
+          existingItem.getItemPrice(), productId, existingItem.getItemQuantity() + itemQuantity);
+      items.put(productId, item);
+    } else {
+      ShoppingItem item = new ShoppingItem(productName, itemPrice, productId, itemQuantity);
+      items.put(productId, item);
+    }
 
   }
 
-  private List<ShoppingItem> items = new ArrayList<>();
-  private BigDecimal totalPrice = new BigDecimal(0).setScale(PRICE_SCALE, BigDecimal.ROUND_HALF_UP);
-  private int itemCount = 0;
+  private BigDecimal calculateItemPrice() {
+    BigDecimal totalPrice = new BigDecimal(0);
+    for (Map.Entry<UUID, ShoppingItem> entry : items.entrySet()) {
+      totalPrice = totalPrice.add(entry.getValue().getItemPrice()
+          .multiply(new BigDecimal(entry.getValue().getItemQuantity())));
+    }
 
-  public void addItem(String productName, BigDecimal itemPrice, UUID productId, int itemQuantity) {
-    ShoppingItem item = new ShoppingItem(productName, itemPrice, productId, itemQuantity);
-    items.add(item);
-    BigDecimal itemCost = (itemPrice
-        .multiply(new BigDecimal(itemQuantity).setScale(PRICE_SCALE, BigDecimal.ROUND_HALF_UP)));
-    totalPrice = totalPrice.add(itemCost);
-    itemCount = itemCount + itemQuantity;
+    return totalPrice.setScale(PRICE_SCALE, BigDecimal.ROUND_HALF_UP);
   }
 
   public BigDecimal getTotalPrice() {
-    return totalPrice;
+    return calculateItemPrice().add(getTaxAmount());
   }
 
-  public List getItems() {
+  public Map<UUID, ShoppingItem> getItems() {
     return items;
   }
 
   public int getItemCount() {
+    int itemCount = 0;
+    for (Map.Entry<UUID, ShoppingItem> entry : items.entrySet()) {
+      itemCount = itemCount + entry.getValue().getItemQuantity();
+    }
+
     return itemCount;
+  }
+
+  public BigDecimal getTaxAmount() {
+    if (taxRate == null) {
+      return BigDecimal.ZERO;
+    } else {
+      return calculateItemPrice().multiply(taxRate)
+          .divide(new BigDecimal(100), 4, BigDecimal.ROUND_HALF_UP)
+          .setScale(PRICE_SCALE, BigDecimal.ROUND_HALF_UP);
+    }
+  }
+
+  public BigDecimal getTaxRate() {
+    return taxRate;
+  }
+
+  public void setTaxRate(BigDecimal taxRate) {
+    this.taxRate = taxRate;
   }
 }
--- src/test/scala/com/ee/eeshoppingcart/AddProductsSpec.scala
+++ src/test/scala/com/ee/eeshoppingcart/AddProductsSpec.scala
@@ -2,7 +2,6 @@ package com.ee.eeshoppingcart
 
 import java.util.UUID
 
-import org.scalatest.Inspectors._
 import org.scalatest.{FeatureSpec, GivenWhenThen, Matchers}
 
 
@@ -25,11 +24,10 @@ class AddProductsSpec extends FeatureSpec with GivenWhenThen with Matchers {
 
       Then("the shopping cart should contain 5 Dove Soaps each with a unit price of 39.99")
       sc.getItemCount should be(5)
-      forAll(sc.getItems) { x => {
-        val item = x.asInstanceOf[ShoppingItem]
-        item.getItemPrice should be(doveSoap.getPrice)
-      }
-      }
+      val expectedPrice = new java.math.BigDecimal(39.99).setScale(ShoppingCart.PRICE_SCALE, java.math.BigDecimal.ROUND_HALF_UP).doubleValue
+      val actualPrice = sc.getItems.get(doveSoapId).getItemPrice.doubleValue
+      actualPrice shouldEqual expectedPrice
+
       And("the shopping cart's total price should equal 199.95")
       val expectedTotalPrice = new java.math.BigDecimal(199.95).setScale(ShoppingCart.PRICE_SCALE, java.math.BigDecimal.ROUND_HALF_UP).doubleValue
       sc.getTotalPrice.doubleValue shouldEqual expectedTotalPrice
@@ -44,18 +42,54 @@ class AddProductsSpec extends FeatureSpec with GivenWhenThen with Matchers {
     val itemPrice = new java.math.BigDecimal(39.99).setScale(ShoppingCart.PRICE_SCALE, java.math.BigDecimal.ROUND_HALF_UP)
     val doveSoap = new Product("Dove Soap", itemPrice, doveSoapId)
 
-    When("The user adds 5 Dove Soaps to the shopping cart")
+    When("the user adds 5 Dove Soaps to the shopping cart")
     sc.addItem(doveSoap.getProductName, doveSoap.getPrice, doveSoap.getProductId, 5)
     And("then adds another 3 Dove Soaps to the shopping cart")
     sc.addItem(doveSoap.getProductName, doveSoap.getPrice, doveSoap.getProductId, 3)
 
-    Then("The shopping cart should contain 8 Dove Soaps each with a unit price of 39.99")
+    Then("the shopping cart should contain 8 Dove Soaps each with a unit price of 39.99")
     sc.getItemCount should be(8)
     And("the shopping cart’s total price should equal 319.92")
     val expectedTotalPrice = new java.math.BigDecimal(319.92).setScale(ShoppingCart.PRICE_SCALE, java.math.BigDecimal.ROUND_HALF_UP).doubleValue
     sc.getTotalPrice.doubleValue shouldEqual expectedTotalPrice
     val shoppingItems = sc.getItems
     val shoppingItems2 = sc.getItems
+  }
+
+  scenario("Step 3: Calculate the tax rate of the shopping cart with multiple items") {
+    Given("An empty shopping cart")
+    val sc = new ShoppingCart()
+    And("a product, Dove Soap with a unit price of 39.99")
+    val doveSoapId = UUID.randomUUID()
+    val doveitemPrice = new java.math.BigDecimal(39.99).setScale(ShoppingCart.PRICE_SCALE, java.math.BigDecimal.ROUND_HALF_UP)
+    val doveSoap = new Product("Dove Soap", doveitemPrice, doveSoapId)
+    And("another product, Axe Deo with a unit price of 99.99")
+    val axeSoapId = UUID.randomUUID()
+    val axeitemPrice = new java.math.BigDecimal(99.99).setScale(ShoppingCart.PRICE_SCALE, java.math.BigDecimal.ROUND_HALF_UP)
+    val axeSoap = new Product("Axe Deo", axeitemPrice, axeSoapId)
+    And("a tax rate of 12.5%")
+    val taxRate = new java.math.BigDecimal(12.5).setScale(ShoppingCart.PRICE_SCALE, java.math.BigDecimal.ROUND_HALF_UP)
+    sc.setTaxRate(taxRate)
+
+    When("the user adds 2 Dove Soaps to the shopping cart")
+    sc.addItem(doveSoap.getProductName, doveSoap.getPrice, doveSoap.getProductId, 2)
+    And("then adds 2 Axe Deo’s to the shopping cart")
+    sc.addItem(axeSoap.getProductName, axeSoap.getPrice, axeSoap.getProductId, 2)
+    Then("The shopping cart should contain 2 Dove Soaps each with a unit price of 39.99")
+    sc.getItems.get(doveSoapId).getItemQuantity should be(2)
+    val expectedUnitPrice = doveitemPrice.doubleValue
+    sc.getItems.get(doveSoapId).getItemPrice.doubleValue shouldEqual expectedUnitPrice
+    And("the shopping cart should contain 2 Axe Deo’s each with a unit price of 99.99")
+    sc.getItems.get(axeSoapId).getItemQuantity should be(2)
+    val expectedAxeUnitPrice = axeitemPrice.doubleValue
+    sc.getItems.get(axeSoapId).getItemPrice.doubleValue shouldEqual expectedAxeUnitPrice
+    And("the total tax amount should equal 35.00")
+    val expectedTaxAmount = new java.math.BigDecimal(35.00).setScale(ShoppingCart.PRICE_SCALE, java.math.BigDecimal.ROUND_HALF_UP).doubleValue
+    sc.getTaxAmount.doubleValue shouldEqual expectedTaxAmount
+    And("the shopping cart’s total price should equal 314.96")
+    val expectedTotalPrice = new java.math.BigDecimal(314.96).setScale(ShoppingCart.PRICE_SCALE, java.math.BigDecimal.ROUND_HALF_UP).doubleValue
+    sc.getTotalPrice.doubleValue shouldEqual expectedTotalPrice
+
   }
 
 }
